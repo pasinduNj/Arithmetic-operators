bb0018b7567bb8828d09df1d302a091b044d27d2
Pasindu Jay <124522613+pasinduNj@users.noreply.github.com>
2024-05-06 03:36:00 +0530

    Add files via upload

---
+++ q1.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+$num1=10;
+$num2=2;
+$sum=$num1+$num2;
+$d=$num1-$num2;
+$p=$num1*$num2;
+$q=$num1**$num2;
+
+echo "The sum is {$sum}<br>";
+echo "The difference is {$d}<br>";
+echo "The product is {$p}<br>";
+echo "The quotient is {$q}<br>";
+
+?>
